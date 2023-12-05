ef86cd7fe58c04428cb486ab6fe474be4537ed48
Choi Sungwook (uid98421) <Sungwook.Choi@vitesco.com>
2023-12-05 11:34:21 +0900

    bom price diff cased by fx rate diff

--- FX_simulation/8. join_bom_fx-rates.py
+++ FX_simulation/8. join_bom_fx-rates.py
@@ -14,24 +14,51 @@ except NameError:
 
 # Filenames
 bom_file = path / "data" / "BOM_price.csv"
-fx_file = path / "data" / "fx_rates_vt.csv"
+fx_vt_file = path / "data" / "fx_rates_VT.csv"
+fx_hmg_file = path / "data" / "fx_rates_HMG.csv"
 sales_file = path / "data" / "Sales with representative PN.csv"
 
-output_file = path / "output" / "output.csv"
+output_file = path / "output" / "bom_price_fx rate_delta.csv"
 
 
 # Read data
 bom = pd.read_csv(bom_file)
-fx = pd.read_csv(fx_file)
+fx_vt = pd.read_csv(fx_vt_file)
+fx_hmg = pd.read_csv(fx_hmg_file)
 sales = pd.read_csv(sales_file)
 
 
 # Functions
-df = pd.merge()
+def filter_fx_vt(df):
+    # Filter rows
+    df = df[df["fx_type"] == "ytd"]
+    df = df[df["month"] == 12]
+    # Select columns
+    df = df[["cur", "year", "fx_rates_VT"]]
+    return df
 
+def filter_fx_hmg(df):
+    # Filter rows    
+    df = df[df["month"] == 12]
+    # Select columns    
+    df = df[["cur", "year", "fx_rates_HMG"]]
+    return df
 
 
 # Process data
+# Filter FX rates
+fx_vt = filter_fx_vt(fx_vt)
+fx_hmg = filter_fx_hmg(fx_hmg)
+
+# Join two dataframes
+df = pd.merge(bom, fx_vt, how="left", on=["cur", "year"])
+df = pd.merge(df, fx_hmg, how="left", on=["cur", "year"])
+df.info()
+
+# Calculate the delta
+df = df[df["cur"] != "KRW"]
+df["fx_rate_diff"] = df["fx_rates_VT"] - df["fx_rates_HMG"]
+df["delta_amt"] = df["amount_doc_"] * df["fx_rate_diff"]
 
 
 # Write data
