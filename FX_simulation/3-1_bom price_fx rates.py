1041a9251359ce612c1b416e0206be818b067a00
Choi Sungwook (uid98421) <Sungwook.Choi@vitesco.com>
2023-12-07 11:19:53 +0900

    ["KRW"] * 12

--- FX_simulation/3-1_bom price_fx rates.py
+++ FX_simulation/3-1_bom price_fx rates.py
@@ -48,20 +48,7 @@ def join_dataframes(base, df1, df2):
 
 
 def krw_month_table():
-    currencies = [
-        "KRW",
-        "KRW",
-        "KRW",
-        "KRW",
-        "KRW",
-        "KRW",
-        "KRW",
-        "KRW",
-        "KRW",
-        "KRW",
-        "KRW",
-        "KRW",
-    ]
+    currencies = ["KRW"] * 12
     months = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
     df = pd.DataFrame({"cur": currencies, "period": months})
     return df
