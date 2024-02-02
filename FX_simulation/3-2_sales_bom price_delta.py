34ccf0615f661f30355bcf02b002363b7766de25
Choi Sungwook (uid98421) <Sungwook.Choi@vitesco.com>
2024-02-02 15:31:12 +0900

    code optimization

--- FX_simulation/3-2_sales_bom price_delta.py
+++ FX_simulation/3-2_sales_bom price_delta.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from pathlib import Path
+import os
 
 
 # Path
@@ -14,29 +15,19 @@ except NameError:
 
 # Filenames
 sales_file = path / "data" / "Sales with representative PN.csv"
-bom_file_1 = path / "output" / "1. bom_price_fx rate_delta.csv"
-bom_file_2 = path / "output" / "2. bom_price_fx rate_delta.csv"
-bom_file_3 = path / "output" / "3. bom_price_fx rate_delta.csv"
-bom_file_4 = path / "output" / "4. bom_price_fx rate_delta.csv"
-bom_file_5 = path / "output" / "5. bom_price_fx rate_delta.csv"
-bom_file_6 = path / "output" / "6. bom_price_fx rate_delta.csv"
-bom_file_7 = path / "output" / "7. bom_price_fx rate_delta.csv"
-bom_file_8 = path / "output" / "8. bom_price_fx rate_delta.csv"
+input_dir = path / "output"
+bom_files = [
+    f for f in os.listdir(input_dir) if f.endswith("bom_price_fx rate_delta.csv")
+]
+bom_files = [input_dir / f for f in bom_files]
+
 output_file = path / "output" / "sales with bom costs.csv"
 output_parquet = path / "output" / "sales with bom costs.parquet"
 
 
 # Read data
 sales = pd.read_csv(sales_file)
-bom_1 = pd.read_csv(bom_file_1)
-bom_2 = pd.read_csv(bom_file_2)
-bom_3 = pd.read_csv(bom_file_3)
-bom_4 = pd.read_csv(bom_file_4)
-bom_5 = pd.read_csv(bom_file_5)
-bom_6 = pd.read_csv(bom_file_6)
-bom_7 = pd.read_csv(bom_file_7)
-bom_8 = pd.read_csv(bom_file_8)
-bom = pd.concat([bom_1, bom_2, bom_3, bom_4, bom_5, bom_6, bom_7, bom_8])
+bom = pd.concat([pd.read_csv(f) for f in bom_files])
 
 
 # Functions
