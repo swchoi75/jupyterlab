9a5ee6c38ebcf35a96f87fcbbb3dc2e740cee482
Choi Sungwook (uid98421) <Sungwook.Choi@vitesco.com>
2024-02-08 11:32:45 +0900

    spending -> aquisition -> depreciation

---
+++ Invest_depreciation/1_process_bud_GPA_spending.py
@@ -0,0 +1,145 @@
+import pandas as pd
+import numpy as np
+from pathlib import Path
+from janitor import clean_names
+
+
+# Path
+try:
+    path = Path(__file__).parent
+except NameError:
+    import inspect
+
+    path = Path(inspect.getfile(lambda: None)).resolve().parent
+
+
+# Variables
+GPA_version = "v379"
+spending_total_col = "plan_spend_2024"
+current_year = "2024"
+current_year_end = pd.to_datetime(current_year + "-12-31")
+
+
+# Filenames
+input_file = path / "bud_data" / "2023_GPA_WMS - All data report_Budget.xlsx"
+meta_file = path / "meta" / "category_of_investment.xlsx"
+master_file = path / "bud_output" / "bud_GPA_master.csv"
+output_file = path / "bud_output" / "bud_monthly_spending_1.csv"
+
+
+# Read data
+df = pd.read_excel(
+    input_file,
+    sheet_name="Sheet1",
+    dtype={
+        "Outlet(Receiver)": str,
+        "Fire-Outlet": str,
+        "FiRe Outlet NY(Receiver)": str,
+        "FiRe plant(Receiver)": str,
+        "Investment type": str,
+    },
+)
+
+df_meta = pd.read_excel(
+    meta_file,
+    sheet_name="Sheet1",
+    usecols="A:F",
+    dtype={"financial_statement_item": str},
+).dropna()
+
+
+# Functions to clean column names
+def clean_new_lines(column_name):
+    return column_name.replace("\n", "")
+
+
+def clean_trailing_underscore(column_name):
+    return column_name.rstrip("_")
+
+
+# Apply the cleaning function to all column names
+df.columns = df.columns.map(clean_new_lines)
+df = df.clean_names()
+df.columns = df.columns.map(clean_trailing_underscore)
+
+
+# Select columns
+key_columns = [
+    "outlet_sender",
+    "outlet_receiver",
+    "categorie_of_investm",
+    "category_of_invest_historic",
+    "fire_outlet",
+    "fire_outlet_ny_receiver",
+    "fire_plant_receiver",
+    "location_receiver",
+    "investment_type",
+    "status",
+    "master",
+    "unnamed_28",
+    "sub",
+    "unnamed_30",
+]
+
+value_columns = df.columns[df.columns.str.contains("spend")].tolist()
+
+df = df[key_columns + value_columns]
+
+
+# Rename columns
+df = df.rename(
+    columns={
+        "categorie_of_investm": "category_of_investment",
+        "unnamed_28": "master_description",
+        "unnamed_30": "sub_description",
+    }
+)
+# Remove the GPA version prefix from each column name
+df.columns = df.columns.str.replace(GPA_version, "")
+
+
+# Filter out missing or zero value
+df = df.dropna(subset="master")
+df = df.dropna(subset=spending_total_col)
+df = df[df[spending_total_col] != 0]
+
+
+# Join two dataframes
+df = df.merge(df_meta, how="left", on="category_of_investment")
+
+
+# Create new column
+df["basic_or_project"] = np.where(
+    df["master_description"].str.contains("Basic", case=False),
+    "basic",
+    "project",
+)
+
+
+# GPA master
+selected_columns = [
+    "outlet_sender",
+    "status",
+    "master",
+    "master_description",
+    "sub",
+    "sub_description",
+    "category_of_investment",
+    "category_description",
+    "gl_account",
+    "gl_account_description",
+    "basic_or_project",
+]
+
+df_master = (
+    df[selected_columns + [spending_total_col]]
+    .groupby(selected_columns)
+    .agg({spending_total_col: "sum"})
+    .reset_index()
+)
+
+
+# Write data
+df_master.to_csv(master_file, index=False)
+df.to_csv(output_file, index=False)
+print("Files are created")
---
+++ Invest_depreciation/2_process_bud_acquisition.py
@@ -0,0 +1,97 @@
+import pandas as pd
+import numpy as np
+from pathlib import Path
+from janitor import clean_names
+
+
+# Path
+try:
+    path = Path(__file__).parent
+except NameError:
+    import inspect
+
+    path = Path(inspect.getfile(lambda: None)).resolve().parent
+
+
+# Filenames
+input_file = path / "fc_output" / "fc_monthly_spending_1.csv"
+meta_file = path / "meta" / "fc_GPA_master.xlsx"
+output_file = path / "fc_output" / "fc_acquisition_future_assets.csv"
+
+
+# Read data
+df = pd.read_csv(
+    input_file,
+    dtype={
+        "outlet_receiver": str,
+        "fire_outlet": str,
+        "fire_outlet_ny_receiver": str,
+        "fire_plant_receiver": str,
+        "investment_type": str,
+        "financial_statement_item": str,
+    },
+)
+
+df_meta = pd.read_excel(
+    meta_file,
+    sheet_name="Manual input",
+    skiprows=3,
+    usecols="E, M:P",
+)
+
+
+# Join two dataframes
+df = df.merge(df_meta, how="left", on="sub")
+
+
+# # Business Logic: Get the last spending months
+# Melt the dataframe
+value_columns = df.columns[df.columns.str.contains("spend")].tolist()
+key_columns = [col for col in df.columns if col not in value_columns]
+
+df = df.melt(
+    id_vars=key_columns,
+    value_vars=value_columns,
+    var_name="spend_month",
+    value_name="spend_fc",
+)
+
+df = df.where(df["spend_fc"] != 0, np.nan)  # turn 0 into n/a values
+df = df.dropna(subset="spend_fc")  # remove rows with n/a values
+
+
+# Aggregate data
+last_months = df[["sub", "spend_month"]].groupby(["sub"]).last()  # .reset_index()
+last_months.rename(columns={"spend_month": "last_spend_month"}, inplace=True)
+
+
+# New column "Acquisition date" based on last spending months
+def str_to_month_ends(series):
+    # Convert year_month to datetime with day set to 1st
+    series = pd.to_datetime(series, format="%Y_%m")
+    # Add one month and subtract one day to get the month end
+    series = series + pd.DateOffset(months=1, days=-1)
+    return series
+
+
+s = last_months["last_spend_month"].str.replace("spend_fc_", "")
+s = str_to_month_ends(s)
+last_months["acquisition_date"] = s
+
+
+# Join two dataframes
+df = df.merge(last_months, how="left", on="sub")
+
+
+# Pivot wider
+df = pd.pivot(
+    df,
+    index=[col for col in df.columns if col not in ["spend_month", "spend_fc"]],
+    columns="spend_month",
+    values="spend_fc",
+).reset_index()
+
+
+# Write data
+df.to_csv(output_file, index=False)
+print("Files are created")
