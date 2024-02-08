180866619929e6dbd964618a89ba31a8ffc1182a
Choi Sungwook (uid98421) <Sungwook.Choi@vitesco.com>
2024-02-08 17:02:11 +0900

    add column "depr_current"

--- Invest_depreciation/3_process_fc_depreciation.py
+++ Invest_depreciation/3_process_fc_depreciation.py
@@ -95,6 +95,11 @@ df = pd.pivot(
 ).reset_index()
 
 
+# Depreciation in current year
+value_columns = df.columns[df.columns.str.contains("2023-")].tolist()
+df["depr_current"] = df[value_columns].sum(axis="columns")
+
+
 # Write data
 df.to_csv(output_file, index=False)
 print("A file is created")
