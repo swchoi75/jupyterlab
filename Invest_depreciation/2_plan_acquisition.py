610d96c752303f03031ef8d42a62413e565f577a
Choi Sungwook (uid98421) <Sungwook.Choi@vitesco.com>
2024-02-19 10:54:10 +0900

    past -> past fc

--- Invest_depreciation/2_plan_acquisition.py
+++ Invest_depreciation/2_plan_acquisition.py
@@ -89,7 +89,7 @@ df["start_of_depr"] = np.where(pd.isna(df["PPAP"]), df["acquisition_date"], df["
 
 # New column: "category" based on the column "start_of_depr"
 df["category"] = np.where(
-    df["start_of_depr"] < pd.to_datetime(actual_month_end), "past", "future"
+    df["start_of_depr"] < pd.to_datetime(actual_month_end), "past fc", "future"
 )
 
 
