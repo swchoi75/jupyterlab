e6e6c23178edf3a91c5c2814e966007db1aa5134
Choi Sungwook (uid98421) <Sungwook.Choi@vitesco.com>
2024-02-23 09:22:13 +0900

    remove xx_receiver columns

--- Invest_depreciation/2_fc_acquisition.py
+++ Invest_depreciation/2_fc_acquisition.py
@@ -30,10 +30,6 @@ output_file = path / "fc_output" / "fc_acquisition_future_assets.csv"
 df = pd.read_csv(
     input_file,
     dtype={
-        "outlet_receiver": str,
-        "fire_outlet": str,
-        "fire_outlet_ny_receiver": str,
-        "fire_plant_receiver": str,
         "investment_type": str,
         "financial_statement_item": str,
     },
