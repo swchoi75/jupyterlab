ca857638684add694ce4f99e939a8a467ab31c06
Choi Sungwook (uid98421) <Sungwook.Choi@vitesco.com>
2024-03-22 15:42:07 +0900

    script folder is created

---
+++ Sales/run_scripts.bat
@@ -0,0 +1,13 @@
+@REM Check python
+where python
+
+@REM Sales
+python script/0_merge_sales_2024.py
+python script/1_sales_overview.py
+
+@REM Sales for BU CT
+python script/2_sales_BU_CT.py
+python script/3_sales_BU_CT_with_meta.py
+python script/4_sales_BU_CT_high_runner.py
+
+
--- Sales/script/3_sales_BU_CT_with_meta.py
+++ Sales/script/3_sales_BU_CT_with_meta.py
@@ -5,11 +5,11 @@ from janitor import clean_names
 
 # Path
 try:
-    path = Path(__file__).parent
+    path = Path(__file__).parent.parent
 except NameError:
     import inspect
 
-    path = Path(inspect.getfile(lambda: None)).resolve().parent
+    path = Path(inspect.getfile(lambda: None)).resolve().parent.parent
 
 
 # Filenames
@@ -25,8 +25,8 @@ output_file = path / "output" / "Sales BU CT_with meta.csv"
 df = pd.read_csv(input_file)
 cg = pd.read_excel(cg_file, usecols="A:B")
 mm = pd.read_excel(mm_file, usecols="A:H").clean_names()
-mm = mm[["material", "material_type","ext_matl_group", "product_hierachy"]]
-mm = mm.rename(columns={"product_hierachy":"product_hierarchy"})
+mm = mm[["material", "material_type", "ext_matl_group", "product_hierachy"]]
+mm = mm.rename(columns={"product_hierachy": "product_hierarchy"})
 ph = pd.read_excel(ph_file, usecols="A:D")
 pg = pd.read_excel(pg_file, usecols="A:B")
 
@@ -34,7 +34,7 @@ pg = pd.read_excel(pg_file, usecols="A:B")
 # Add customer_group
 df = df.merge(cg, how="left", on="sold_to_name_1")
 
-              
+
 # Add material_type & product hierarchy
 df = df.merge(mm, how="left", left_on="product", right_on="material")
 df = df.drop(columns=["material"])
@@ -45,7 +45,7 @@ df = df.merge(ph, how="left", on="product_hierarchy")
 
 
 # Apply string slicing to get product_group
-df['product_group'] = df['product_hierarchy'].str[2:5].str.extract(r'([A-Z]\d{2})')
+df["product_group"] = df["product_hierarchy"].str[2:5].str.extract(r"([A-Z]\d{2})")
 
 
 # Add productline out of product_group
@@ -53,7 +53,7 @@ df = df.merge(pg, how="left", on="product_group")
 
 
 # Extract HMG customer part number
-pattern = r'\b([A-Z0-9]{5}-[A-Z0-9]{5})\b'
+pattern = r"\b([A-Z0-9]{5}-[A-Z0-9]{5})\b"
 df["HMG_PN"] = df["matnr_descr_"].str.extract(pattern)
 
 
