050a1b07dd66d0aba78d92d877e25c37021f8a39
Choi Sungwook (uid98421) <Sungwook.Choi@vitesco.com>
2024-03-29 09:21:35 +0900

    folder name change

--- Invest_depreciation/run_scripts.bat
+++ Invest_depreciation/run_scripts.bat
@@ -2,25 +2,25 @@
 where python
 
 @REM Preprocessing
-python src/0_derive_cc_and_prj.py
+python script/0_derive_cc_and_prj.py
 
 @REM GPA spending
-python src/1_fc_GPA_spending.py
+python script/1_fc_GPA_spending.py
 
 @REM Acquisition
-python src/2_fc_acquisition.py
-python src/2_act_SAP_acquisition.py
+python script/2_fc_acquisition.py
+python script/2_act_SAP_acquisition.py
 
 @REM 
-python src/3_adjustment_manually.py
-python src/3_export_additional_data.py
+python script/3_adjustment_manually.py
+python script/3_export_additional_data.py
 
 @REM Depreciation
-python src/4_fc_depreciation.py
-python src/4_act_depreciation.py
-python src/4_auc_depreciation.py
-python src/4_merge_versions.py
+python script/4_fc_depreciation.py
+python script/4_act_depreciation.py
+python script/4_auc_depreciation.py
+python script/4_merge_versions.py
 
 @REM Central Function Allocation
-python src/5_central_function_allocation.py
-python src/5_reorder_columns.py
+python script/5_central_function_allocation.py
+python script/5_reorder_columns.py
