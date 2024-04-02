6d3cde6e9a7fd306722c484fef0514fd8178ec76
Choi Sungwook (uid98421) <Sungwook.Choi@vitesco.com>
2024-04-02 16:39:07 +0900

    update variable in "common_variable.py"

--- Invest_depreciation/script/common_variable.py
+++ Invest_depreciation/script/common_variable.py
@@ -4,12 +4,12 @@ import pandas as pd
 # Variables
 
 # Monthly update
-actual_month_end = "2023-11-30"
-asset_filename = "2023-11_Asset History Leger_20231130.xlsx"
-GPA_filename = "2023-11_GPA_WMS - All data report_FC.xlsx"
 GPA_version = "v380"
+actual_month_end = "2023-11-30"
 
 # Yearly update
+asset_filename = "2023-11_Asset History Leger_20231130.xlsx"
+GPA_filename = "2023-11_GPA_WMS - All data report_FC.xlsx"
 spending_total_col = "spend_fc_2023"
 current_year = "2023"
 current_year_end = pd.to_datetime(current_year + "-12-31")
