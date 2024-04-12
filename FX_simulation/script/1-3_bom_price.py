2d423c137aa02997c6345e93743d103637660547
Choi Sungwook (uid98421) <Sungwook.Choi@vitesco.com>
2024-04-12 14:59:01 +0900

    update on FP style

--- FX_simulation/script/1-3_bom_price.py
+++ FX_simulation/script/1-3_bom_price.py
@@ -12,33 +12,43 @@ except NameError:
     path = Path(inspect.getfile(lambda: None)).resolve().parent.parent
 
 
-# Filenames
-input_file = path / "data" / "BOM.parquet"
-output_file = path / "data" / "BOM_price.csv"
+# Functions
+def filter_out_curr(df, currency="CNY"):
+    return df[df["cur"] != currency]
 
 
-# Read data
-df = pd.read_parquet(input_file)
+def add_ICO_or_NOT(df):
+    df["outs_ic"] = np.where(
+        (df["component_no"] == "AAA2043950000") & (df["cur"] == "USD"), "IC", "FR"
+    )
+    return df
 
 
-# Filter data
-df = df[df["cur"] != "CNY"]
+def aggregate_data(df):
+    df = (
+        df.groupby(["year", "product", "cur", "outs_ic"], dropna=False)
+        .agg({"total_amount_org_cur_": "sum"})
+        .reset_index()
+    )
+    return df
 
 
-# Add ICO column
-df["outs_ic"] = np.where(
-    (df["component_no"] == "AAA2043950000") & (df["cur"] == "USD"), "IC", "FR"
-)
+def main():
+
+    # Filenames
+    input_file = path / "data" / "BOM.parquet"
+    output_file = path / "data" / "BOM_price.csv"
+
+    # Read data
+    df = pd.read_parquet(input_file)
+
+    # Process data
+    df = df.pipe(filter_out_curr, "CNY").pipe(add_ICO_or_NOT).pipe(aggregate_data)
+
+    # Write data
+    df.to_csv(output_file, index=False)
+    print("A file is created.")
 
 
-# Aggregate data
-df = (
-    df.groupby(["year", "product", "cur", "outs_ic"], dropna=False)
-    .agg({"total_amount_org_cur_": "sum"})
-    .reset_index()
-)
-
-
-# Write data
-df.to_csv(output_file, index=False)
-print("A file is created.")
+if __name__ == "__main__":
+    main()
