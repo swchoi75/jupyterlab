c15fdce133a18b20c698d6d65e2ca200fd5eddf2
Choi Sungwook (uid98421) <Sungwook.Choi@vitesco.com>
2024-04-12 09:00:30 +0900

    update on FP style

--- Invest_depreciation/script/4_fc_depreciation.py
+++ Invest_depreciation/script/4_fc_depreciation.py
@@ -9,36 +9,34 @@ from common_variable import period_start, period_end
 
 
 # Path
-path = Path(__file__).parent.parent
+try:
+    path = Path(__file__).parent.parent
+except NameError:
+    import inspect
 
-
-# Filenames
-input_file = path / "output" / "3_fc_acquisition_future_assets_adj.csv"
-output_file = path / "output" / "4_fc_depreciation_future_assets.csv"
-
-
-# Read data
-df = pd.read_csv(
-    input_file,
-    dtype={
-        "investment_type": str,
-        "financial_statement_item": str,
-        "input_cost_center": str,
-    },
-    parse_dates=["acquisition_date", "PPAP", "start_of_depr"],
-)
-
-
-# # Business Logic: Monthly deprecation # #
-
-
-# Dataframe for month end dates
-df_month_ends = pd.DataFrame(
-    {"month_ends": pd.date_range(period_start, period_end, freq="ME")}
-)
+    path = Path(inspect.getfile(lambda: None)).resolve().parent.parent
 
 
 # Functions
+def read_data(filename):
+    df = pd.read_csv(
+        filename,
+        dtype={
+            "investment_type": str,
+            "financial_statement_item": str,
+            "input_cost_center": str,
+        },
+        parse_dates=["acquisition_date", "PPAP", "start_of_depr"],
+    )
+    return df
+
+
+def calc_depr_end(row):
+    years = row["useful_life_year"]
+    row = row["start_of_depr"] + pd.DateOffset(years=years)
+    return row
+
+
 def calc_monthly_depr(row):
     if row["useful_life_year"] == 0:
         return 0
@@ -46,10 +44,20 @@ def calc_monthly_depr(row):
     return monthly_depr
 
 
-def calc_depr_end(row):
-    years = row["useful_life_year"]
-    row = row["start_of_depr"] + pd.DateOffset(years=years)
-    return row
+def add_col_monthly_depr(df):
+    """Create new columns"""
+    df["depr_start"] = df["start_of_depr"]
+    df["depr_end"] = df.apply(calc_depr_end, axis="columns")
+    df["monthly_depr"] = df.apply(calc_monthly_depr, axis="columns")
+    return df
+
+
+def month_table_df():
+    """Dataframe for month end dates"""
+    df = pd.DataFrame(
+        {"month_ends": pd.date_range(period_start, period_end, freq="ME")}
+    )
+    return df
 
 
 def filter_depr_periods(df):
@@ -60,33 +68,50 @@ def filter_depr_periods(df):
     return df
 
 
-# Create new columns
-df["depr_start"] = df["start_of_depr"]
-df["depr_end"] = df.apply(calc_depr_end, axis="columns")
-df["monthly_depr"] = df.apply(calc_monthly_depr, axis="columns")
+def pivot_wider(df):
+    df["month_ends"] = df["month_ends"].astype(str)
+    df = pd.pivot(
+        df,
+        index=[col for col in df.columns if col not in ["month_ends", "monthly_depr"]],
+        columns="month_ends",
+        values="monthly_depr",
+    ).reset_index()
+    return df
 
 
-# Cross Join & apply function "filter_depr_periods"
-df = df_month_ends.join(df, how="cross")
-df = filter_depr_periods(df)
+def add_col_depr_current(df):
+    """New column: Depreciation in current year as sum of monthly values"""
+    pattern = re.compile(r"\d{4}-\d{2}-\d{2}")
+    value_columns = df.columns[df.columns.str.contains(pattern)].tolist()
+    df["depr_current"] = df[value_columns].sum(axis="columns")
+    return df
 
 
-# Pivot_wider
-df["month_ends"] = df["month_ends"].astype(str)
-df = pd.pivot(
-    df,
-    index=[col for col in df.columns if col not in ["month_ends", "monthly_depr"]],
-    columns="month_ends",
-    values="monthly_depr",
-).reset_index()
+def main():
+
+    # Filenames
+    input_file = path / "output" / "3_fc_acquisition_future_assets_adj.csv"
+    output_file = path / "output" / "4_fc_depreciation_future_assets.csv"
+
+    # Process data
+    df = (
+        read_data(input_file)
+        # add new cols: monthly_depr, depr_start, depr_end
+        .pipe(add_col_monthly_depr)
+    )
+    df_month_table = month_table_df()
+    df = df_month_table.join(df, how="cross")
+    df = (
+        # Make monthly_depr values into zero if the periods are not in depr_periods
+        df.pipe(filter_depr_periods)
+        # new col: depr_current as sum of monthly values
+        .pipe(pivot_wider).pipe(add_col_depr_current)
+    )
+
+    # Write data
+    df.to_csv(output_file, index=False)
+    print("A file is created")
 
 
-# New column: Depreciation in current year
-pattern = re.compile(r"\d{4}-\d{2}-\d{2}")
-value_columns = df.columns[df.columns.str.contains(pattern)].tolist()
-df["depr_current"] = df[value_columns].sum(axis="columns")
-
-
-# Write data
-df.to_csv(output_file, index=False)
-print("A file is created")
+if __name__ == "__main__":
+    main()
