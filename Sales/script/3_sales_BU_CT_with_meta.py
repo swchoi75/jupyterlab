1f72bc8975ff2672f64641d9e24691840be25205
Choi Sungwook (uid98421) <Sungwook.Choi@vitesco.com>
2024-04-12 14:13:28 +0900

    update on FP style

--- Sales/script/3_sales_BU_CT_with_meta.py
+++ Sales/script/3_sales_BU_CT_with_meta.py
@@ -12,51 +12,48 @@ except NameError:
     path = Path(inspect.getfile(lambda: None)).resolve().parent.parent
 
 
-# Filenames
-input_file = path / "output" / "Sales BU CT.csv"
-mm_file = path / "meta" / "material_master_BU CT.xlsx"
-cg_file = path / "meta" / "customer_group.xlsx"
-ph_file = path / "meta" / "product_hierarchy_BU CT.xlsx"
-pg_file = path / "meta" / "product_group_BU CT.xlsx"
-output_file = path / "output" / "Sales BU CT_with meta.csv"
+def main():
+    # Filenames
+    input_file = path / "output" / "Sales BU CT.csv"
+    mm_file = path / "meta" / "material_master_BU CT.xlsx"
+    cg_file = path / "meta" / "customer_group.xlsx"
+    ph_file = path / "meta" / "product_hierarchy_BU CT.xlsx"
+    pg_file = path / "meta" / "product_group_BU CT.xlsx"
+    output_file = path / "output" / "Sales BU CT_with meta.csv"
+
+    # Read data
+    df = pd.read_csv(input_file)
+    cg = pd.read_excel(cg_file, usecols="A:B")
+    mm = pd.read_excel(mm_file, usecols="A:H").clean_names()
+    mm = mm[["material", "material_type", "ext_matl_group", "product_hierachy"]]
+    mm = mm.rename(columns={"product_hierachy": "product_hierarchy"})
+    ph = pd.read_excel(ph_file, usecols="A:D")
+    pg = pd.read_excel(pg_file, usecols="A:B")
+
+    # Add customer_group
+    df = df.merge(cg, how="left", on="sold_to_name_1")
+
+    # Add material_type & product hierarchy
+    df = df.merge(mm, how="left", left_on="product", right_on="material")
+    df = df.drop(columns=["material"])
+
+    # Add PH description
+    df = df.merge(ph, how="left", on="product_hierarchy")
+
+    # Apply string slicing to get product_group
+    df["product_group"] = df["product_hierarchy"].str[2:5].str.extract(r"([A-Z]\d{2})")
+
+    # Add productline out of product_group
+    df = df.merge(pg, how="left", on="product_group")
+
+    # Extract HMG customer part number
+    pattern = r"\b([A-Z0-9]{5}-[A-Z0-9]{5})\b"
+    df["HMG_PN"] = df["matnr_descr_"].str.extract(pattern)
+
+    # Write data
+    df.to_csv(output_file, index=False)
+    print("A file is created.")
 
 
-# Read data
-df = pd.read_csv(input_file)
-cg = pd.read_excel(cg_file, usecols="A:B")
-mm = pd.read_excel(mm_file, usecols="A:H").clean_names()
-mm = mm[["material", "material_type", "ext_matl_group", "product_hierachy"]]
-mm = mm.rename(columns={"product_hierachy": "product_hierarchy"})
-ph = pd.read_excel(ph_file, usecols="A:D")
-pg = pd.read_excel(pg_file, usecols="A:B")
-
-
-# Add customer_group
-df = df.merge(cg, how="left", on="sold_to_name_1")
-
-
-# Add material_type & product hierarchy
-df = df.merge(mm, how="left", left_on="product", right_on="material")
-df = df.drop(columns=["material"])
-
-
-# Add PH description
-df = df.merge(ph, how="left", on="product_hierarchy")
-
-
-# Apply string slicing to get product_group
-df["product_group"] = df["product_hierarchy"].str[2:5].str.extract(r"([A-Z]\d{2})")
-
-
-# Add productline out of product_group
-df = df.merge(pg, how="left", on="product_group")
-
-
-# Extract HMG customer part number
-pattern = r"\b([A-Z0-9]{5}-[A-Z0-9]{5})\b"
-df["HMG_PN"] = df["matnr_descr_"].str.extract(pattern)
-
-
-# Write data
-df.to_csv(output_file, index=False)
-print("A file is created.")
+if __name__ == "__main__":
+    main()
--- Sales/script/4_sales_BU_CT_high_runner.py
+++ Sales/script/4_sales_BU_CT_high_runner.py
@@ -12,87 +12,116 @@ except NameError:
     path = Path(inspect.getfile(lambda: None)).resolve().parent.parent
 
 
-# Filenames
-input_file = path / "output" / "Sales BU CT_with meta.csv"
-output_file = path / "output" / "Sales high runner per PH.csv"
-result_file = path / "output" / "Sales high runner PN.csv"
-full_sales = path / "output" / "Sales with representative PN.csv"
+# Functions
+def aggregate_data(df):
+    df = (
+        df.groupby(
+            [
+                "fy",
+                "recordtype",
+                "product_hierarchy",
+                "PH_description",
+                "customer_engines",
+                "customer_products",
+                "product",
+                "material_type",
+                "ext_matl_group",
+                "product_group",
+                "productline",
+                "customer_group",
+                "HMG_PN",
+            ],
+            dropna=False,  # To avoid deleting rows with missing values
+        )
+        .agg({"quantity": "sum", "totsaleslc": "sum"})
+        .reset_index()
+    )
+    return df
 
 
-# Read data
-df = pd.read_csv(input_file)
+def filter_data(df):
+    # Filter data for Representative PN
+    df = df[df["recordtype"] == "F"]
+    df = df[df["material_type"].isin(["FERT", "HALB"])]
+    df = df[(df["totsaleslc"] > 5e7) | (df["totsaleslc"] < -5e7)]  # over +/- 50M KRW
+    return df
 
 
-# Aggregate data
-df = (
-    df.groupby(
+def sort_data(df):
+    df = df.sort_values(
+        by=["fy", "product_hierarchy", "totsaleslc"], ascending=[True, True, False]
+    )
+    return df
+
+
+def filter_first_occurrence(df):
+    # Filter the first occurrence of each category
+    df = df.groupby(["fy", "product_hierarchy"]).head(1)
+    return df
+
+
+def full_data(df, filename):
+    # Full sales data
+    df = pd.read_csv(filename).drop(
+        columns="HMG_PN"  # to be replaced by HMG_PN of representative PN
+    )
+    return df
+
+
+def representative_df(df):
+    df = df[
         [
+            # Key
             "fy",
             "recordtype",
             "product_hierarchy",
-            "PH_description",
-            "customer_engines",
-            "customer_products",
+            # Representative PN and HMG_PN
             "product",
-            "material_type",
-            "ext_matl_group",
-            "product_group",
-            "productline",
-            "customer_group",
             "HMG_PN",
-        ],
-        dropna=False,  # To avoid deleting rows with missing values
+        ]
+    ].rename(columns={"product": "representative_pn"})
+    return df
+
+
+def add_representative_pn(df, df_reprsentative_pn):
+    df = df.merge(
+        df_reprsentative_pn, how="left", on=["fy", "recordtype", "product_hierarchy"]
     )
-    .agg({"quantity": "sum", "totsaleslc": "sum"})
-    .reset_index()
-)
+    return df
 
 
-# Filter data for Representative PN
-df = df[df["recordtype"] == "F"]
-df = df[df["material_type"].isin(["FERT", "HALB"])]
-df = df[(df["totsaleslc"] > 5e7) | (df["totsaleslc"] < -5e7)]  # over +/- 50M KRW
+def filter_material_type(df):
+    # Remove representative_pn if material type is HAWA or ROH
+    df["representative_pn"] = df["representative_pn"].where(
+        df["material_type"].isin(["FERT", "HALB"])
+    )
+    return df
 
 
-# Sort data
-df = df.sort_values(
-    by=["fy", "product_hierarchy", "totsaleslc"], ascending=[True, True, False]
-)
+def main():
+
+    # Filenames
+    input_file = path / "output" / "Sales BU CT_with meta.csv"
+    output_file = path / "output" / "Sales high runner per PH.csv"
+    result_file = path / "output" / "Sales high runner PN.csv"
+    full_sales = path / "output" / "Sales with representative PN.csv"
+
+    # Read data
+    df = pd.read_csv(input_file)
+    df = df.pipe(aggregate_data).pipe(filter_data).pipe(sort_data)
+    result = df.pipe(filter_first_occurrence)
+    df_representative = representative_df(result)
+    df_full = df.pipe(full_data, input_file)
+    df_full = df_full.pipe(add_representative_pn, df_representative).pipe(
+        filter_material_type
+    )
+
+    # Write data
+    df.to_csv(output_file, index=False)
+    result.to_csv(result_file, index=False)
+    df_full.to_csv(full_sales, index=False)
+    print("files are created.")
 
 
-# Filter the first occurrence of each category
-result = df.groupby(["fy", "product_hierarchy"]).head(1)
-
-
-# Full sales data
-df_full = pd.read_csv(input_file).drop(
-    columns="HMG_PN"  # to be replaced by HMG_PN of representative PN
-)
-
-representative = result[
-    [
-        # Key
-        "fy",
-        "recordtype",
-        "product_hierarchy",
-        # Representative PN and HMG_PN
-        "product",
-        "HMG_PN",
-    ]
-].rename(columns={"product": "representative_pn"})
-
-df_full = df_full.merge(
-    representative, how="left", on=["fy", "recordtype", "product_hierarchy"]
-)
-
-# Remove representative_pn if material type is HAWA or ROH
-df_full["representative_pn"] = df_full["representative_pn"].where(
-    df_full["material_type"].isin(["FERT", "HALB"])
-)
-
-
-# Write data
-df.to_csv(output_file, index=False)
-result.to_csv(result_file, index=False)
-df_full.to_csv(full_sales, index=False)
-print("files are created.")
+if __name__ == "__main__":
+    main()
