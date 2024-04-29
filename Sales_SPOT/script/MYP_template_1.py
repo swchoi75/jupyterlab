9b59e94c3eb42b66215acc70916011463f95be23
Choi Sungwook (uid98421) <Sungwook.Choi@vitesco.com>
2024-04-29 16:30:27 +0900

    two scripts in xlsxwriter and openpyxl

--- Sales_SPOT/script/MYP_template_1.py
+++ Sales_SPOT/script/MYP_template_1.py
@@ -12,7 +12,7 @@ def add_text(worksheet):
     worksheet.write("C6", "Instruction / comments")
 
 
-def add_items(list_of_data, worksheet):
+def add_items(worksheet, list_of_data):
     # Start from the cell. Rows and columns are zero indexed.
     row = 0
     col = 3
@@ -25,7 +25,7 @@ def add_items(list_of_data, worksheet):
         row += 1
 
 
-def add_poc(list_of_data, worksheet):
+def add_poc(worksheet, list_of_data):
     # Start from the cell. Rows and columns are zero indexed.
     row = 1
     col = 4
@@ -51,7 +51,7 @@ def add_poc(list_of_data, worksheet):
         col += 9
 
 
-def add_years(list_of_data, worksheet):
+def add_years(worksheet, list_of_data):
     # Start from the cell. Rows and columns are zero indexed.
     rows = [5, 47, 76, 104, 122, 134, 143]
 
@@ -62,7 +62,7 @@ def add_years(list_of_data, worksheet):
             col += 1
 
 
-def add_zero(list_of_data, worksheet):
+def add_zero(worksheet, list_of_data):
     # Start from the cell. Rows and columns are zero indexed.
     rows = (
         list(range(7, 45))
@@ -79,25 +79,49 @@ def add_zero(list_of_data, worksheet):
             col += 1
 
 
-def add_formula(worksheet):
+def add_formula_vt(worksheet):
     # Dynamic array
-    worksheet.write_dynamic_array_formula("E50:L50", "=E8:L8")
-    worksheet.write_dynamic_array_formula("E51:L51", "=E9:L9")
-    worksheet.write_dynamic_array_formula("E52:L52", "=E15:L15")
+    worksheet.write_dynamic_array_formula("E50:L50", "=E8:L8")  # Sales
+    worksheet.write_dynamic_array_formula("E51:L51", "=E9:L9")  # Therein ICO Sales
+    worksheet.write_dynamic_array_formula("E52:L52", "=E15:L15")  # Sales variations
     worksheet.write_dynamic_array_formula("E53:L53", "=E8:L8+E10:L10+E12:L12+E13:L13")
     worksheet.write_dynamic_array_formula(
         "E54:L54", "=IF(ISERROR(E53:L53/E50:L50),0,(E53:L53/E50:L50))"
-    )
+    )  # % of sales
     worksheet.write_dynamic_array_formula("E55:L55", "=E53:L53+E16:L16")
     worksheet.write_dynamic_array_formula(
         "E56:L56", "=IF(ISERROR(E55:L55/E50:L50),0,(E55:L55/E50:L50))"
-    )
+    )  # % of sales
     worksheet.write_dynamic_array_formula("E57:L57", "=E22:L22+E30:L30")
     worksheet.write_dynamic_array_formula("E58:L58", "=E28:L28+E29:L29")
     worksheet.write_dynamic_array_formula("E59:L59", "=E31:L31")
     worksheet.write_dynamic_array_formula("E60:L60", "=E55:L55+E57:L57+E59:L59")
     worksheet.write_dynamic_array_formula(
         "E61:L61", "=IF(ISERROR(E60:L60/E50:L50),0,(E60:L60/E50:L50))"
+    )  # % of sales
+    worksheet.write_dynamic_array_formula("E62:L62", "=E32:L32+E33:L33+E34:L34+E35:L35")
+    worksheet.write_dynamic_array_formula("E63:L63", "=E33:L33+E34:L34+E35:L35")
+    worksheet.write_dynamic_array_formula("E64:L64", "=E36:L36+E37:L37+E38:L38")
+    worksheet.write_dynamic_array_formula("E65:L65", "=E37:L37+E38:L38")
+    worksheet.write_dynamic_array_formula("E66:L66", "=E39:L39+E40:L40+E41:L41")
+    worksheet.write_dynamic_array_formula("E67:L67", "=E40:L40+E41:L41")
+    worksheet.write_dynamic_array_formula("E68:L68", "=E42:L42")
+    worksheet.write_dynamic_array_formula("E69:L69", "=E43:L43")
+    worksheet.write_dynamic_array_formula("E70:L70", "=E44:L44")
+    worksheet.write_dynamic_array_formula("E71:L71", "=E45:L45")
+    worksheet.write_dynamic_array_formula(
+        "E72:L72", "=E60:L60+E62:L62+E64:L64+E66:L66+E68:L68+E69:L69+E70:L70+E71:L71"
+    )  # Earnings before interest and tax (EBIT)
+    worksheet.write_dynamic_array_formula(
+        "E73:L73", "=IF(ISERROR(E72:L72/E50:L50),0,(E72:L72/E50:L50))"
+    )  # % of sales
+
+
+def add_formula_sa(worksheet):
+    # Dynamic array
+    worksheet.write_dynamic_array_formula("E79:L79", "=E50:L50-E51:L51-E52:L52")
+    worksheet.write_dynamic_array_formula(
+        "E79:L79", "(E10:L10+E12:L12+E13:L13+E16:L16+E22:L22+E30:L30+E31:L31)*-1"
     )
 
 
@@ -127,13 +151,14 @@ def main():
     with xlsxwriter.Workbook(output_file) as workbook:
         worksheets = [workbook.add_worksheet(name) for name in sheet_names]
 
-        for worksheet in worksheets:
-            add_text(worksheet)
-            add_items(items, worksheet)
-            add_poc(data, worksheet)
-            add_years(fy, worksheet)
-            add_zero(zero, worksheet)
-            add_formula(worksheet)
+        for ws in worksheets:
+            add_text(ws)
+            add_items(ws, items)
+            add_poc(ws, data)
+            add_years(ws, fy)
+            add_zero(ws, zero)
+            add_formula_vt(ws)
+            add_formula_sa(ws)
 
     print("A file is created")
 
--- Sales_SPOT/script/MYP_template_2.py
+++ Sales_SPOT/script/MYP_template_2.py
@@ -1,5 +1,5 @@
 import csv
-import xlsxwriter
+import openpyxl as op
 from pathlib import Path
 
 
@@ -9,103 +9,83 @@ path = Path(__file__).parent.parent
 
 # Functions
 def add_text(worksheet):
-    worksheet.write("C6", "Instruction / comments")
+    # ws["C6"] = "Instruction / comments"
+    worksheet.cell(row=6, column=3).value = "Instruction / comments"
 
 
-def add_items(list_of_data, worksheet):
+def add_items(worksheet, list_of_data):
     # Start from the cell. Rows and columns are zero indexed.
-    row = 0
-    col = 3
+    row = 1
+    col = 4
 
     # Write the data from the CSV file, handling empty lines
     for data in list_of_data:
         # Check if the row is not empty before accessing data[0]
         if data:
-            worksheet.write(row, col, data[1])  # items
+            worksheet.cell(row, col).value = data[1]  # items
         row += 1
 
 
-def add_poc(list_of_data, worksheet):
-    # Start from the cell. Rows and columns are zero indexed.
-    row = 1
-    col = 4
-
-    for data in list_of_data:
-        worksheet.write(row, col, data[0])  # Division
-        col += 9
-
+def add_poc(worksheet, list_of_data):
     # Start from the cell. Rows and columns are zero indexed.
     row = 2
-    col = 4
+    col = 5
 
     for data in list_of_data:
-        worksheet.write(row, col, data[1])  # BU
+        worksheet.cell(row, col).value = data[0]  # Division
         col += 9
 
     # Start from the cell. Rows and columns are zero indexed.
     row = 3
-    col = 4
+    col = 5
 
     for data in list_of_data:
-        worksheet.write(row, col, data[2])  # Productlines
+        worksheet.cell(row, col).value = data[1]  # BU
+        col += 9
+
+    # Start from the cell. Rows and columns are zero indexed.
+    row = 4
+    col = 5
+
+    for data in list_of_data:
+        worksheet.cell(row, col).value = data[2]  # Productlines
         col += 9
 
 
-def add_years(list_of_data, worksheet):
+def add_years(worksheet, list_of_data):
     # Start from the cell. Rows and columns are zero indexed.
-    rows = [5, 47, 76, 104, 122, 134, 143]
+    rows = [6, 48, 77, 105, 123, 135, 144]
 
     for row in rows:
-        col = 4
+        col = 5
         for data in list_of_data:
-            worksheet.write(row, col, data)
+            worksheet.cell(row, col).value = data
             col += 1
 
 
-def add_zero(list_of_data, worksheet):
+def add_zero(worksheet, list_of_data):
     # Start from the cell. Rows and columns are zero indexed.
     rows = (
-        list(range(7, 45))
-        + list(range(105, 113))
-        + list(range(123, 130))
-        + list(range(135, 138))
-        + list(range(144, 147))
+        list(range(8, 45 + 1))
+        + list(range(106, 113 + 1))
+        + list(range(124, 130 + 1))
+        + list(range(136, 138 + 1))
+        + list(range(145, 147 + 1))
     )
 
     for row in rows:
-        col = 4
+        col = 5
         for data in list_of_data:
-            worksheet.write(row, col, data)
+            worksheet.cell(row, col).value = data
             col += 1
-
-
-def add_formula(worksheet):
-    # Dynamic array
-    worksheet.write_dynamic_array_formula("E50:L50", "=E8:L8")
-    worksheet.write_dynamic_array_formula("E51:L51", "=E9:L9")
-    worksheet.write_dynamic_array_formula("E52:L52", "=E15:L15")
-    worksheet.write_dynamic_array_formula("E53:L53", "=E8:L8+E10:L10+E12:L12+E13:L13")
-    worksheet.write_dynamic_array_formula(
-        "E54:L54", "=IF(ISERROR(E53:L53/E50:L50),0,(E53:L53/E50:L50))"
-    )
-    worksheet.write_dynamic_array_formula("E55:L55", "=E53:L53+E16:L16")
-    worksheet.write_dynamic_array_formula(
-        "E56:L56", "=IF(ISERROR(E55:L55/E50:L50),0,(E55:L55/E50:L50))"
-    )
-    worksheet.write_dynamic_array_formula("E57:L57", "=E22:L22+E30:L30")
-    worksheet.write_dynamic_array_formula("E58:L58", "=E28:L28+E29:L29")
-    worksheet.write_dynamic_array_formula("E59:L59", "=E31:L31")
-    worksheet.write_dynamic_array_formula("E60:L60", "=E55:L55+E57:L57+E59:L59")
-    worksheet.write_dynamic_array_formula(
-        "E61:L61", "=IF(ISERROR(E60:L60/E50:L50),0,(E60:L60/E50:L50))"
-    )
 
 
 def main():
     # Filenames
     input_items = path / "data" / "items.csv"
     input_pl = path / "data" / "PLs.csv"
-    output_file = path / "output" / "MYP_template.xlsx"
+    input_file = path / "data" / "MYP consolidation template.xlsx"
+    output_file = path / "output" / "MYP_template_op.xlsx"
     sheet_names = ["Korea", "India", "Japan", "Thailand"]
 
     # Read data
@@ -124,18 +104,23 @@ def main():
     zero = [0, 0, 0, 0, 0, 0, 0, 0, ""] * 27  # no. of section
 
     # Create an Excel workbook
-    with xlsxwriter.Workbook(output_file) as workbook:
-        worksheets = [workbook.add_worksheet(name) for name in sheet_names]
+    wb = op.Workbook()
+    ws = wb.active
 
-        for worksheet in worksheets:
-            add_text(worksheet)
-            add_items(items, worksheet)
-            add_poc(data, worksheet)
-            add_years(fy, worksheet)
-            add_zero(zero, worksheet)
-            add_formula(worksheet)
+    add_text(ws)
+    add_items(ws, items)
+    add_poc(ws, data)
+    add_years(ws, fy)
+    add_zero(ws, zero)
+
+    wb.save(output_file)
 
     print("A file is created")
+
+    # Read from an Excel workbook
+    # wb = op.load_workbook(input_file)
+    # ws_list = wb.sheetnames
+    # print(ws_list)
 
 
 if __name__ == "__main__":
