973579493785b3f3c07e572fa4e8bce6d00a79c2
Choi Sungwook (uid98421) <Sungwook.Choi@vitesco.com>
2024-05-09 17:12:42 +0900

    batch file to run python scripts for MYP

---
+++ Sales_SPOT/run_scripts_MYP.bat
@@ -0,0 +1,9 @@
+@REM Check python
+where python
+
+@REM Running the scripts for MYP
+python script/2-1_read_MYP_figures.py
+python script/2-2_process_MYP_figures.py
+python script/2-3_derive_cf_allo_ratio.py
+python script/2-4_cf_allocation.py
+python script/2-5_final_MYP.py
