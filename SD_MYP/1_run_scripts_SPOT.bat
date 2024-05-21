38ebf649117cf7c3fb322ac81d9f0e6cde110c06
Choi Sungwook (uid98421) <Sungwook.Choi@vitesco.com>
2024-05-22 08:29:59 +0900

    apply SG fx rate

--- SD_MYP/1_run_scripts_SPOT.bat
+++ SD_MYP/1_run_scripts_SPOT.bat
@@ -5,5 +5,6 @@ where python
 python script/1-1_SPOT_Div_P.py
 python script/1-2_SPOT_Div_E.py
 python script/1-3_combine_P_and_E.py
-python script/1-4_final_SPOT.py
+python script/1-4_reshape_SPOT.py
+python script/1-5_apply_FX_rate.py
 
