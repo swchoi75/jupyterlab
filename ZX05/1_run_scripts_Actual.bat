3166dffe034ebc4757e6561ae3c5033fea3c54a4
Choi Sungwook (uid98421) <Sungwook.Choi@vitesco.com>
2024-06-03 17:29:14 +0900

    add batch file to run multiple python scripts

---
+++ ZX05/1_run_scripts_Actual.bat
@@ -0,0 +1,10 @@
+@REM Check python
+where python
+
+@REM Running the scripts for Actual
+python script/0_create_db.py
+python script/1_actual_central_function.py
+python script/1_actual_productline_fix.py
+python script/1_actual_productline_var.py
+@REM python script/1_optional_actual_overtime.py
+
