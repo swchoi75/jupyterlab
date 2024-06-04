eb3d2b52468f96f4ef246300771b65eca56140d8
Choi Sungwook (uid98421) <Sungwook.Choi@vitesco.com>
2024-06-04 10:16:47 +0900

    minor update

--- Invest_depreciation/script/common_variable.py
+++ Invest_depreciation/script/common_variable.py
@@ -14,9 +14,10 @@ if version == "FC":
     asset_filename = "2023-11_Asset History Leger_20231130.xlsx"
     GPA_filename = "2023-11_GPA_WMS - All data report_FC.xlsx"
     result_filename = "fc_depreciation_this_year.csv"
-    #
+    # Text manipulation
     spending_total_col = "spend_fc_2023"
     text_to_remove = "spend_fc_"
+    # Reclassify to AuC
     current_year = "2023"
     current_year_end = pd.to_datetime(current_year + "-12-31")
     # Depreciation period
@@ -28,10 +29,11 @@ elif version == "Plan":
     asset_filename = "2023-11_Asset History Leger_20231130.xlsx"
     GPA_filename = "2023-11_GPA_WMS - All data report_Plan.xlsx"
     result_filename = "fc_depreciation_next_year.csv"
-    #
+    # Text manipulation
     spending_total_col = "spend_fc_2023"
     text_to_remove = "spend_fc_"
-    current_year = "2023"
+    # Reclassify to AuC
+    current_year = "2024"
     current_year_end = pd.to_datetime(current_year + "-12-31")
     # Depreciation period
     period_start = "2024-01-01"  # 1월초, 즉 yyyy-01-01
