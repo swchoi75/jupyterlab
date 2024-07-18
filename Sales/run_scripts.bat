ce0679689affd42c4ba03d9827e414baadb4b8bb
Choi Sungwook (uid98421) <Sungwook.Choi@vitesco.com>
2024-07-18 11:30:46 +0900

    update run_scripts

--- Sales/run_scripts.bat
+++ Sales/run_scripts.bat
@@ -2,10 +2,10 @@
 where python
 
 @REM Sales
-python script/0_merge_sales_2024.py
+python script/0_merge_copa_sales.py
 python script/1_sales_overview.py
 
-@REM Sales for BU CT
+@REM Sales for Division Electrification
 python script/2_sales_Div_E.py
 python script/3_sales_Div_E_with_meta.py
 python script/4_sales_Div_E_high_runner.py
