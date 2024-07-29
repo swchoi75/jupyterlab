cdf83f098eebfa8d1228ca51e80df538e401fb48
Choi Sungwook (uid98421) <Sungwook.Choi@vitesco.com>
2024-07-29 14:19:25 +0900

    price_impact

--- Sales_P3/src/dplyr/price_impact.R
+++ Sales_P3/src/dplyr/price_impact.R
@@ -1,43 +1,50 @@
 library(dplyr)
 library(tidyr)
+library(readr)
+library(here)
 
 
-# Calculate Sales Price impact
+# Path
+path <- here("Sales_P3")
+
+
+# Functions
 calculate_price_impact <- function(df) {
   df <- df |>
     mutate(
-      act_price = ifelse(df$Version == "Budget" |
-          df$Qty == 0, 0,
-        round(df$Sales_LC / df$Qty, 0)
+      act_price = ifelse(.data$version == "Budget" |
+          .data$qty == 0, 0,
+        round(.data$sales_lc / .data$qty, 0)
       ),
-      price_diff = ifelse(df$Qty == 0, 0,
-        df$act_price - df$bud_price
+      price_diff = ifelse(.data$qty == 0, 0,
+        .data$act_price - .data$bud_price
       ),
       price_impact = case_when(
 
         # Business logic
-        df["CM Cluster"] == "OES" ~ 0, # OES
-        df$Product == "A2C1797520201" ~ 0, # Kappa HEV adjustment
-        df$Product == "A2C1636530101" ~ 0, # Kappa HEV adjustment
+        .data$cm_cluster == "OES" ~ 0, # OES
+        .data$product == "A2C1797520201" ~ 0, # Kappa HEV adjustment
+        .data$product == "A2C1636530101" ~ 0, # Kappa HEV adjustment
 
         # General formula
-        df$Qty == 0 ~ df$Sales_LC,
-        is.na(df$Qty) ~ df$Sales_LC,
-        TRUE ~ df$Qty * df$price_diff
+        .data$qty == 0 ~ .data$sales_lc,
+        is.na(.data$qty) ~ .data$sales_lc,
+        TRUE ~ .data$qty * .data$price_diff
       ),
-      price_impact_ratio = df$price_impact / df$Sales_LC * 100,
+      price_impact_ratio = .data$price_impact / .data$sales_lc * 100,
     )
   return(df)
 }
 
-# Replace missing values with zero for numeric columns
+
 replace_missing_values <- function(df) {
+  # Replace missing values with zero for numeric columns
   df <- df |>
     mutate(
-      Qty = replace_na(df$Qty, 0),
-      Sales_LC = replace_na(df$Sales_LC, 0),
-      STD_Costs = replace_na(df$STD_Costs, 0),
-      `Standard Price` = replace_na(df["Standard Price"], 0),
+      qty = replace_na(df$qty, 0),
+      sales_lc = replace_na(df$sales_lc, 0),
+      std_costs = replace_na(df$std_costs, 0),
+      standard_price = replace_na(df$standard_price, 0),
       bud_price = replace_na(df$bud_price, 0),
       act_price = replace_na(df$act_price, 0),
       price_diff = replace_na(df$price_diff, 0),
@@ -46,3 +53,25 @@ replace_missing_values <- function(df) {
     )
   return(df)
 }
+
+
+main <- function() {
+  # Filenames
+  input_file <- here(path, "output", "6_ytd_sales_spv_mapping.csv")
+  output_file <- here(path, "output", "7_ytd_sales_price_impact.csv")
+
+  # Read data
+  df <- read_csv(input_file, show_col_types = FALSE)
+
+  # Process data
+  df <- df |>
+    calculate_price_impact() |>
+    replace_missing_values()
+
+  # Write data
+  write_csv(df, output_file)
+
+  print("A file is created")
+}
+
+main()
--- Sales_P3/src/dplyr/price_vol_mix.R
+++ Sales_P3/src/dplyr/price_vol_mix.R
@@ -1,43 +1,122 @@
 library(dplyr)
 library(readr)
+library(janitor)
+library(here)
 
 
-join_with_cm_ratio <- function(df) {
-  bud_cm_ratio <- read_csv("meta/Budget Contribution Margin ratio.csv",
-    show_col_types = FALSE
-  ) |>
-    select(c("Profit Ctr", "CM ratio"))
+# Path
+path <- here("Sales_P3")
 
+
+# Functions
+add_cm_ratio <- function(df, df_meta) {
   df <- df |>
-    left_join(bud_cm_ratio, by = "Profit Ctr")
+    left_join(df_meta, by = "profit_ctr")
   return(df)
 }
 
-delta_impact <- function(df) {
+
+add_delta_sales <- function(df) {
   df <- df |>
     mutate(
-      # Sales
-      `delta Sales` = ifelse(df$Version == "Actual",
-        df$Sales_LC,
-        df$Sales_LC * -1
-      ),
-      `delta Sales Price` = ifelse(df$Version == "Actual",
-        df$price_impact,
-        df$price_impact * -1
-      ),
-      `delta Sales Volume` =
-        df["delta Sales"] - df["delta Sales Price"],
-
-      # Margin
-      `delta Margin` = ifelse(df$Version == "Actual",
-        df$Sales_LC - df$STD_Costs,
-        (df$Sales_LC - df$STD_Costs) * -1
-      ),
-      `delta Margin Price` = df["delta Sales Price"],
-      `delta Margin Volume` =
-        df["delta Sales Volume"] * df["CM ratio"] / 100,
-      `delta Margin Mix` = df["delta Margin"] -
-      df["delta Margin Price"] - df["delta Margin Volume"]
+      delta_sales = ifelse(.data$version == "Actual",
+        .data$sales_lc,
+        .data$sales_lc * -1
+      )
     )
   return(df)
 }
+
+
+add_delta_sales_price <- function(df) {
+  df <- df |>
+    mutate(
+      delta_sales_price = ifelse(.data$version == "Actual",
+        .data$price_impact,
+        .data$price_impact * -1
+      )
+    )
+  return(df)
+}
+
+
+add_delta_sales_vol <- function(df) {
+  df <- df |>
+    mutate(
+      delta_sales_volume = .data$delta_sales - .data$delta_sales_price
+    )
+  return(df)
+}
+
+
+add_delta_margin <- function(df) {
+  df <- df |>
+    mutate(
+      delta_margin = ifelse(.data$version == "Actual",
+        .data$sales_lc - .data$std_costs,
+        (.data$sales_lc - .data$std_costs) * -1
+      )
+    )
+  return(df)
+}
+
+
+add_delta_margin_price <- function(df) {
+  df <- df |>
+    mutate(
+      delta_margin_price = .data$delta_sales_price
+    )
+  return(df)
+}
+
+
+add_delta_margin_vol <- function(df) {
+  df <- df |>
+    mutate(
+      delta_margin_volume =
+        .data$delta_sales_volume * .data$cm_ratio / 100
+    )
+  return(df)
+}
+
+
+add_delta_margin_mix <- function(df) {
+  df <- df |>
+    mutate(
+      delta_margin_mix = .data$delta_margin -
+      .data$delta_margin_price - .data$delta_margin_volume
+    )
+  return(df)
+}
+
+
+main <- function() {
+  # Filenames
+  input_file <- here(path, "output", "7_ytd_sales_price_impact.csv")
+  meta_file <- here(path, "meta", "Budget Contribution Margin ratio.csv")
+  output_file <- here(path, "output", "ytd_sales_p3_impact.csv")
+
+  # Read data
+  df <- read_csv(input_file, show_col_types = FALSE)
+  df_meta <- read_csv(meta_file, show_col_types = FALSE) |>
+    select(c("Profit Ctr", "CM ratio")) |>
+    clean_names()
+
+  # Process data
+  df <- df |>
+    add_cm_ratio(df_meta) |>
+    add_delta_sales() |>
+    add_delta_sales_price() |>
+    add_delta_sales_vol() |>
+    add_delta_margin() |>
+    add_delta_margin_price() |>
+    add_delta_margin_vol() |>
+    add_delta_margin_mix()
+
+  # Write data
+  write_csv(df, output_file)
+
+  print("A file is created")
+}
+
+main()
